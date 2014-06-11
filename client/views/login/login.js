57c3394d27aca257ef8e88cd9ccc03d53889fd63
Chris Magnacca <chrismagnacca@gmail.com>
2014-06-11 16:17:51 -0400

    updated login.js to display error on failed logins

--- client/views/login/login.js
+++ client/views/login/login.js
@@ -1,7 +1,8 @@
 Template.login.events({
-  'submit #login-form' : function(e, t){
+  'submit form#login-form' : function(e, t){
     e.preventDefault();
-
+    var $flash = $("div.flash");
+    $flash.hide();
     var email = t.find('#login-email').value
     var password = t.find('#login-password').value;
 
@@ -9,8 +10,16 @@ Template.login.events({
       Meteor.loginWithPassword(email, password, function(error) {
         if (error) {
           // user login has failed due to bad password or email
+          var $msg = $("div#message");
+          var message = "Sorry, ".concat(error.reason);
+
+          $msg.text(message);
+          $flash.css("background", "#F2DEDE");
+          $flash.toggle();
         } else {
           // user has been logged in.
+          $flash.hide();
+          $("#modal").modal("hide");
         }
       });
     else
