af3ed3886dd315d8bd0a9b32a505879738ab0d6a
Chris Magnacca <chrismagnacca@gmail.com>
2014-08-05 12:21:51 -0400

    removing unused folders

---
+++ client/lib/helpers/highcharts/meteor_methods.js
@@ -0,0 +1,17 @@
+Meteor.methods({
+	PieChart: function() {
+		return Highcharts.charts[0];
+	},
+
+	UpdateSeriesData: function() {
+		var seriesData = [];
+		var options = Options.find({});
+
+		options.forEach(function(option) {
+			var dataPoint = [option.name, option.votes];
+			seriesData.push(dataPoint);
+		});
+
+		Meteor.call("PieChart").series[0].setData(seriesData, true);
+	}
+});
