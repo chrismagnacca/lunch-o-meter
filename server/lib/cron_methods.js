2de28a0c9599ab4bbf26971d396943116e1a0d28
Chris Magnacca <chris.magnacca@climate.com>
2015-05-28 19:53:43 -0700

    updating server startup to not publish user data collection

--- server/lib/cron_methods.js
+++ server/lib/cron_methods.js
@@ -8,21 +8,21 @@
 // │ └─────────────────────── month of year (1-12)
 // └───────────────────────── day of week   (0-7 is Sun, or use names)
 var archive = function() {
-	// store current day data series & reset options data for next day
-	var seriesData = [];
-	var options = Options.find({});
+  // store current day data series & reset options data for next day
+  var seriesData = [];
+  var options = Options.find({});
 
-	options.forEach(function(option) {
-		var dataPoint = [option.name, option.votes];
-		seriesData.push(dataPoint);
-	});
+  options.forEach(function(option) {
+  var dataPoint = [option.name, option.votes];
+    seriesData.push(dataPoint);
+  });
 
-	var currentTime = new Date();
-	Archives.insert({ series: seriesData, date: currentTime });
+  var currentTime = new Date();
+  Archives.insert({ series: seriesData, date: currentTime });
 };
 
 var Cron = new Meteor.Cron({
-	events: {
-		"* * * * *" : archive
-	}
+  events: {
+    "0 0 * * *" : archive
+  }
 });
--- server/startup/default.js
+++ server/startup/default.js
@@ -1,2 +1,10 @@
 Meteor.startup(function () {
+  /**
+   * ensure User data cannot be updated via Client
+   */
+  Meteor.users.deny({
+    update: function() {
+      return true;
+    }
+  });
 });
